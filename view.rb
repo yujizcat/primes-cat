46ad2faddce8112a70cf557f5442443ed55905d0
yuji zhou <yujizcat@outlook.com>
2022-05-17 19:27:06 +0800

    add timer and calculate score

--- view.rb
+++ view.rb
@@ -26,19 +26,23 @@ class View
     @current_run = true
 
     # -------Test only--------
-    # @primes_game.change_cards(@game_id, [2, 3, 5])
+    @primes_game.change_cards(@game_id, [2, 3, 5])
     # ------------------------
 
     @original = @primes_game.get_player_cards(@current_player).clone
 
     # pause
+    start_time = Time.now
     while @current_run == true
       system "clear"
 
       # ------------Test score-------------
-      # osum = 100
-      # rnd = 20
-      # p (osum ** 2) / (rnd + 1 + osum / 10)
+      #osum = 100
+      #olen = 4
+      #rnd = 16
+      #t = 40
+      #p (((osum ** Math.cbrt(olen) ** 0.9) / (rnd * 4 + t / 4)) ** 1.5).to_i
+      #exit
       # -----------------------------------
 
       # p "Lucky Number: #{@primes_game.get_lucky_number}"
@@ -50,7 +54,7 @@ class View
       p "Average: #{@primes_game.get_player_cards_average(@current_player)}"
       puts ""
 
-      win if @primes_game.get_player_cards(@current_player).size <= 1
+      win(start_time) if @primes_game.get_player_cards(@current_player).size <= 1
 
       @primes_game.prompt_add(@current_player)
       @primes_game.auto_reduce_fraction(@current_player)
@@ -61,18 +65,21 @@ class View
     end
   end
 
-  def game_points_calculate
-    score = (@original.sum ** 2) / (@round + @original.sum / 10)
+  def game_points_calculate(total_time)
+    score = (((@original.sum ** Math.cbrt(@original.sum.size) ** 0.9) / (@round * 4 + total_time / 4)) ** 1.5).to_i
     return 1 if score < 1
     return score
   end
 
-  def win
+  def win(start_time)
+    end_time = Time.now
+    total_time = end_time - start_time
     system "clear"
     puts "You Win!"
     puts "Original cards: #{@original}"
     puts "Rounds took: #{@round - 1}"
-    puts "Your score: #{game_points_calculate}"
+    puts "Time took: #{total_time.round(2)}s"
+    puts "Your score: #{game_points_calculate(total_time).to_i}"
     exit
   end
 end
